1b45a252dcf423242fc72e2d0daace0b96b9ae3a
Abhishek8252 <abhishekchavan323@gmail.com>
2024-10-06 21:11:22 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('abhi8252')
+})
+
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${process.env.port}`)
+//   console.log(process.env.port)
+})
